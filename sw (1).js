c00231ef13538340b965c7aeccb96d247b505c5a
bra83 <55473935+bra83@users.noreply.github.com>
2026-01-12 02:07:09 -0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,38 @@
+const CACHE_NAME = '3d-erp-v23-final';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Estratégia Network-First para evitar ficar preso em cache velho durante debug
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
